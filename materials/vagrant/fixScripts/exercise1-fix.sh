6fa695d9243b45d790c8ef4997d61be702e7ce19
CyrilBaah <cbaah123@gmail.com>
2023-08-14 20:44:58 +0000

    update DNS resolver configuration

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise1 here
+
+# Update DNS resolver configuration
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+# Retry the curl command after fixing the DNS configuration
+curl http://www.textfiles.com/art/bnbascii.txt
