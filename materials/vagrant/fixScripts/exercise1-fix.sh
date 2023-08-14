ecc2133ca82b5d6cca5706e46310624a0c27323d
CyrilBaah <cbaah123@gmail.com>
2023-08-14 21:18:03 +0000

    grant access | exercise3

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #add fix to exercise1 here
 
-# Update DNS resolver configuration
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-
-# Retry the curl command after fixing the DNS configuration
 curl http://www.textfiles.com/art/bnbascii.txt
