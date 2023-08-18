c0857594af4e8622d42d59e7560a8cb5fe2b0b3e
CyrilBaah <cbaah123@gmail.com>
2023-08-18 12:10:34 +0000

    exercise 5 with updates on exercise 5

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
 
-echo "192.168.60.11 server2" >> /etc/hosts
+sudo sed -i '$ a 192.168.60.11 server2' /etc/hosts
--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
 
-echo "192.168.60.10 server1" >> /etc/hosts
+sudo sed -i '$ a 192.168.60.10 server1' /etc/hosts
